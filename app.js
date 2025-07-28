7887b537439640ca89cbd36f7e1fbb018ad4d82f
carlos.henrique <carloshenriquesw@gmail.com>
2025-07-28 18:51:51 -0300

    feat: function exibitTextoNaTela implementado

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
-function verificarChute() {
-  console.log('O botão foi clicado!');
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
+
