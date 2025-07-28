15eea2ea882ba35f1f57ae90720d5bae8cbb70a6
carlos.henrique <carloshenriquesw@gmail.com>
2025-07-28 19:04:31 -0300

    refactor: indentação do codigo e otimização da estrutura em geral

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
@@ -34,26 +34,27 @@ function verificarChute() {
         tentativas++;
         limparCampo();
     }
+}
 
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
 
-    function gerarNumeroAleatorio() {
-      let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
-      let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
 
-      if (quantidadeDeElementosNaLista == numeroLimite) {
-          listaDeNumerosSorteados = [];
-      }
-      if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
-          return gerarNumeroAleatorio();
-      } else {
-          listaDeNumerosSorteados.push(numeroEscolhido);
-          console.log(listaDeNumerosSorteados)
-          return numeroEscolhido;
-      }
-  }
-
-  function limparCampo() {
-    chute = document.querySelector('input');
+function limparCampo() {
+    let chute = document.querySelector('input');
     chute.value = '';
 }
 
@@ -62,7 +63,5 @@ function reiniciarJogo() {
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
-}
-
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
 }
